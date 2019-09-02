62dbb739c35acc68f6a7ad292404f766c715dc42
Jared Forth <jaredforthdev@gmail.com>
2019-09-02 15:14:30 -0500

    modify evaluation functions to use lvalue type

--- parsing.c
+++ parsing.c
@@ -84,28 +84,38 @@ void lval_print(lval v) {
 void lval_println(lval v) { lval_print(v); putchar('\n'); }
 
 /* Use operator string to see which operation to perform */
-long eval_op(long x, char* op, long y) {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    return 0;
-}
+lval eval_op(lval x, char* op, lval y) {
 
-long eval(mpc_ast_t* t) {
+    /* If either value is an error return it */
+    if (x.type == LVAL_ERR) { return x; }
+    if (y.type == LVAL_ERR) { return y; }
 
-    /* If tagged as number return it directly. */
-    if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+    /* Otherwise do maths on the number values */
+    if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+    if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+    if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+    if (strcmp(op, "/") == 0) {
+        /* If second operand is zero return error */
+        return y.num == 0
+               ? lval_err(LERR_DIV_ZERO)
+               : lval_num(x.num / y.num);
     }
 
-    /* The operator is always second child. */
+    return lval_err(LERR_BAD_OP);
+}
+
+lval eval(mpc_ast_t* t) {
+
+    if (strstr(t->tag, "number")) {
+        /* Check if there is some error in conversion */
+        errno = 0;
+        long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+    }
+
     char* op = t->children[1]->contents;
+    lval x = eval(t->children[2]);
 
-    /* We store the third child in `x` */
-    long x = eval(t->children[2]);
-
-    /* Iterate the remaining children and combining. */
     int i = 3;
     while (strstr(t->children[i]->tag, "expr")) {
         x = eval_op(x, op, eval(t->children[i]));
@@ -145,8 +155,8 @@ int main(int argc, char** argv) {
         /* Attempt to Parse the user Input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_println(result);
             mpc_ast_delete(r.output);
 
         } else {
