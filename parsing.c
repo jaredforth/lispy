7aebfdd80e4b81d781221dae33980206c316b0a9
Jared Forth <jaredforthdev@gmail.com>
2019-09-02 14:19:51 -0500

    printf attempt to parse user input

--- parsing.c
+++ parsing.c
@@ -55,7 +55,18 @@ int main(int argc, char** argv) {
         char* input = readline("lispy> ");
         add_history(input);
 
-        printf("No you're a %s\n", input);
+        /* Attempt to Parse the user Input */
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            /* On Success Print the AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            /* Otherwise Print the Error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
         free(input);
 
     }
