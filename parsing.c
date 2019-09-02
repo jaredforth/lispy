944e50900a4fe821fe96ef63ebcf5602bad2dd5f
Jared Forth <jaredforthdev@gmail.com>
2019-09-02 15:29:37 -0500

    update parser to accomodate s-expression

--- parsing.c
+++ parsing.c
@@ -127,21 +127,21 @@ lval eval(mpc_ast_t* t) {
 
 int main(int argc, char** argv) {
 
-    /* Create Some Parsers */
-    mpc_parser_t* Number   = mpc_new("number");
-    mpc_parser_t* Operator = mpc_new("operator");
-    mpc_parser_t* Expr     = mpc_new("expr");
-    mpc_parser_t* Lispy    = mpc_new("lispy");
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Symbol = mpc_new("symbol");
+    mpc_parser_t* Sexpr  = mpc_new("sexpr");
+    mpc_parser_t* Expr   = mpc_new("expr");
+    mpc_parser_t* Lispy  = mpc_new("lispy");
 
-/* Define them with the following Language */
     mpca_lang(MPCA_LANG_DEFAULT,
-              "                                         \
-    number   : /-?[0-9]+/ ;                             \
-    operator : '+' | '-' | '*' | '/' ;                  \
-    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+              "                                          \
+    number : /-?[0-9]+/ ;                    \
+    symbol : '+' | '-' | '*' | '/' ;         \
+    sexpr  : '(' <expr>* ')' ;               \
+    expr   : <number> | <symbol> | <sexpr> ; \
+    lispy  : /^/ <expr>* /$/ ;               \
   ",
-              Number, Operator, Expr, Lispy);
+              Number, Symbol, Sexpr, Expr, Lispy);
 
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
@@ -169,7 +169,7 @@ int main(int argc, char** argv) {
     }
 
     /* Undefine and Delete our Parsers */
-    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
 
     return 0;
 }
