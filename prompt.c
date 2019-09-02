1a6ffcc4379b12eb87c44db712bbdb590d9eb262
Jared Forth <jaredforthdev@gmail.com>
2019-09-02 13:55:07 -0500

    add readline lib

--- prompt.c
+++ prompt.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char** argv) {
 
-  /* Print Version and Exit Information */
-  puts("Cadence Version 0.0.0.0.1");
-  puts("Press Ctrl+C to Exit\n");
+    /* Print Version and Exit Information */
+    puts("Lispy Version 0.0.0.0.1");
+    puts("Press Ctrl+c to Exit\n");
 
-  /* In a never ending loop */
-  while (1) {
+    /* In a never ending loop */
+    while (1) {
 
-    /* Output our prompt */
-    fputs("cadence> ", stdout);
+        /* Output our prompt and get input */
+        char* input = readline("lispy> ");
 
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+        /* Add input to history */
+        add_history(input);
 
-    /* Echo input back to user */
-    printf("No you're a %s", input);
-  }
+        /* Echo input back to user */
+        printf("No you're a %s\n", input);
 
-  return 0;
+        /* Free retrieved input */
+        free(input);
+
+    }
+
+    return 0;
 }
