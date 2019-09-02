2c6324c6bdc1f2a08cae7ea660634a60b2c56637
Jared Forth <jaredforthdev@gmail.com>
2019-09-02 14:17:04 -0500

    implement first parser

--- parsing.c
+++ parsing.c
@@ -30,6 +30,22 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
+    /* Create Some Parsers */
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Lispy    = mpc_new("lispy");
+
+/* Define them with the following Language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+              "                                         \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+              Number, Operator, Expr, Lispy);
+
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
 
@@ -43,6 +59,9 @@ int main(int argc, char** argv) {
         free(input);
 
     }
+
+    /* Undefine and Delete our Parsers */
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
