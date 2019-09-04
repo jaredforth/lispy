bbf72b452f5554b7b3f5c0c931c12bbb28140755
Jared Forth <jaredforthdev@gmail.com>
2019-09-04 15:55:26 -0500

    add put function

--- parsing.c
+++ parsing.c
@@ -222,6 +222,32 @@ lval* lenv_get(lenv* e, lval* k) {
     return lval_err("unbound symbol!");
 }
 
+void lenv_put(lenv* e, lval* k, lval* v) {
+
+    /* Iterate over all items in environment */
+    /* This is to see if variable already exists */
+    for (int i = 0; i < e->count; i++) {
+
+        /* If variable is found delete item at that position */
+        /* And replace with variable supplied by user */
+        if (strcmp(e->syms[i], k->sym) == 0) {
+            lval_del(e->vals[i]);
+            e->vals[i] = lval_copy(v);
+            return;
+        }
+    }
+
+    /* If no existing entry found allocate space for new entry */
+    e->count++;
+    e->vals = realloc(e->vals, sizeof(lval*) * e->count);
+    e->syms = realloc(e->syms, sizeof(char*) * e->count);
+
+    /* Copy contents of lval and symbol string into new location */
+    e->vals[e->count-1] = lval_copy(v);
+    e->syms[e->count-1] = malloc(strlen(k->sym)+1);
+    strcpy(e->syms[e->count-1], k->sym);
+}
+
 void lval_print(lval* v);
 
 void lval_expr_print(lval* v, char open, char close) {
