f80ef71282b1527320715aab0acdc1c1d80e1003
Jared Forth <jaredforthdev@gmail.com>
2019-09-04 15:04:58 -0500

    update symbol to include a variety of characters for variable names

--- parsing.c
+++ parsing.c
@@ -372,14 +372,13 @@ int main(int argc, char** argv) {
     mpc_parser_t* Lispy  = mpc_new("lispy");
 
     mpca_lang(MPCA_LANG_DEFAULT,
-              "                                                        \
-    number : /-?[0-9]+/ ;                                  \
-    symbol : \"list\" | \"head\" | \"tail\"                \
-           | \"join\" | \"eval\" | '+' | '-' | '*' | '/' ; \
-    sexpr  : '(' <expr>* ')' ;                             \
-    qexpr  : '{' <expr>* '}' ;                             \
-    expr   : <number> | <symbol> | <sexpr> | <qexpr> ;     \
-    lispy  : /^/ <expr>* /$/ ;                             \
+              "                                                     \
+    number : /-?[0-9]+/ ;                               \
+    symbol : /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;         \
+    sexpr  : '(' <expr>* ')' ;                          \
+    qexpr  : '{' <expr>* '}' ;                          \
+    expr   : <number> | <symbol> | <sexpr> | <qexpr> ;  \
+    lispy  : /^/ <expr>* /$/ ;                          \
   ",
               Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
 
