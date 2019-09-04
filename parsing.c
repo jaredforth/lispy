2ede23e6f741a9ec82d292887ad20695091aaa42
Jared Forth <jaredforthdev@gmail.com>
2019-09-04 13:46:19 -0500

    add Q Expression

--- parsing.c
+++ parsing.c
@@ -269,6 +269,7 @@ int main(int argc, char** argv) {
     mpc_parser_t* Number = mpc_new("number");
     mpc_parser_t* Symbol = mpc_new("symbol");
     mpc_parser_t* Sexpr  = mpc_new("sexpr");
+    mpc_parser_t* Qexpr  = mpc_new("qexpr");
     mpc_parser_t* Expr   = mpc_new("expr");
     mpc_parser_t* Lispy  = mpc_new("lispy");
 
@@ -280,7 +281,7 @@ int main(int argc, char** argv) {
       expr   : <number> | <symbol> | <sexpr> ; \
       lispy  : /^/ <expr>* /$/ ;               \
     ",
-              Number, Symbol, Sexpr, Expr, Lispy);
+              Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
 
     puts("Lispy Version 0.0.0.0.5");
     puts("Press Ctrl+c to Exit\n");
@@ -305,7 +306,7 @@ int main(int argc, char** argv) {
 
     }
 
-    mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
+    mpc_cleanup(5, Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
 
     return 0;
 }
