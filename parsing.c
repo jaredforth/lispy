21f0daffdfbe7c51b2073d9fe7b2b74669532ab1
Jared Forth <jaredforthdev@gmail.com>
2019-09-04 16:21:19 -0500

    builtins for environment

--- parsing.c
+++ parsing.c
@@ -497,6 +497,28 @@ lval* builtin(lval* a, char* func) {
     return lval_err("Unknown Function!");
 }
 
+void lenv_add_builtin(lenv* e, char* name, lbuiltin func) {
+    lval* k = lval_sym(name);
+    lval* v = lval_fun(func);
+    lenv_put(e, k, v);
+    lval_del(k); lval_del(v);
+}
+
+void lenv_add_builtins(lenv* e) {
+    /* List Functions */
+    lenv_add_builtin(e, "list", builtin_list);
+    lenv_add_builtin(e, "head", builtin_head);
+    lenv_add_builtin(e, "tail", builtin_tail);
+    lenv_add_builtin(e, "eval", builtin_eval);
+    lenv_add_builtin(e, "join", builtin_join);
+
+    /* Mathematical Functions */
+    lenv_add_builtin(e, "+", builtin_add);
+    lenv_add_builtin(e, "-", builtin_sub);
+    lenv_add_builtin(e, "*", builtin_mul);
+    lenv_add_builtin(e, "/", builtin_div);
+}
+
 int main(int argc, char** argv) {
 
     mpc_parser_t* Number = mpc_new("number");
