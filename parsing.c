2f901f89594c5de52c55f68292e7f1dfb25bfdcf
Jared Forth <jaredforthdev@gmail.com>
2019-09-04 16:32:01 -0500

    update lval error function

--- parsing.c
+++ parsing.c
@@ -63,11 +63,26 @@ lval* lval_num(long x) {
 }
 
 /* Construct a pointer to a new Error lval */
-lval* lval_err(char* m) {
+lval* lval_err(char* fmt, ...) {
     lval* v = malloc(sizeof(lval));
     v->type = LVAL_ERR;
-    v->err = malloc(strlen(m) + 1);
-    strcpy(v->err, m);
+
+    /* Create a va list and initialize it */
+    va_list va;
+    va_start(va, fmt);
+
+    /* Allocate 512 bytes of space */
+    v->err = malloc(512);
+
+    /* printf the error string with a maximum of 511 characters */
+    vsnprintf(v->err, 511, fmt, va);
+
+    /* Reallocate to number of bytes actually used */
+    v->err = realloc(v->err, strlen(v->err)+1);
+
+    /* Cleanup our va list */
+    va_end(va);
+
     return v;
 }
 
