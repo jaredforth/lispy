9c07307b097c95ea89e69480e4268d66df1b5143
Jared Forth <jforth22@gmail.com>
2019-09-04 12:34:47 -0500

    add second lval read function

--- parsing.c
+++ parsing.c
@@ -179,6 +179,28 @@ lval* lval_read_num(mpc_ast_t* t) {
            lval_num(x) : lval_err("invalid number");
 }
 
+lval* lval_read(mpc_ast_t* t) {
+
+    /* If Symbol or Number return conversion to that type */
+    if (strstr(t->tag, "number")) { return lval_read_num(t); }
+    if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+    /* If root (>) or sexpr then create empty list */
+    lval* x = NULL;
+    if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+    if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
+
+    /* Fill this list with any valid expression contained within */
+    for (int i = 0; i < t->children_num; i++) {
+        if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+        if (strcmp(t->children[i]->tag,  "regex") == 0) { continue; }
+        x = lval_add(x, lval_read(t->children[i]));
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
 
     mpc_parser_t* Number = mpc_new("number");
