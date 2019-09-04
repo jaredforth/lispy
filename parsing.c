e64d13a3404316e1d5aefd9c9beea631ec4614ec
Jared Forth <jforth22@gmail.com>
2019-09-04 12:45:21 -0500

    revert

--- parsing.c
+++ parsing.c
@@ -292,9 +292,10 @@ int main(int argc, char** argv) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            lval* x = lval_read(r.output);
+            lval* x = lval_eval(lval_read(r.output));
             lval_println(x);
             lval_del(x);
+            mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
