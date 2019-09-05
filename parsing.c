4b3712694239a3078f43168418c901aa34b5274d
Jared Forth <jaredforthdev@gmail.com>
2019-09-05 17:00:28 -0500

    add forward declarations

--- parsing.c
+++ parsing.c
@@ -27,6 +27,15 @@ struct lenv;
 typedef struct lval lval;
 typedef struct lenv lenv;
 
+mpc_parser_t* Number;
+mpc_parser_t* Symbol;
+mpc_parser_t* String;
+mpc_parser_t* Comment;
+mpc_parser_t* Sexpr;
+mpc_parser_t* Qexpr;
+mpc_parser_t* Expr;
+mpc_parser_t* Lispy;
+
 /* Lisp Value */
 
 enum { LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_STR,
